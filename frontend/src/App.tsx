6d820c3df27e8c4624bd369b58ceaceab7653cc1
Apolat <apolatbusiness@gmail.com>
2025-08-16 15:07:58 +0200

    Frontend Website design finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
+import XLogo from './img/x.png';
+import LinkedInLogo from './img/linkedin.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World!</p>
+        <h1>Anime Quote Generator</h1>
       </header>
       <main className="App-main">
-        <p>Welcome to the React application.</p>
-        <p>This is a simple example of a React component.</p> 
-
+        <div className="quote-container">
+          <p>“Knowing what it feels to be in pain, is exactly why we try to be kind to others.”</p>
+          <h2>Jiraiya (Naruto)</h2>
+        </div>
+        <button className="quote-button">Get Another Quote</button>
       </main>
       <footer className="App-footer">
-        <p>© 2023 My React App</p>
+        <img className='social-icons' src={XLogo} alt="A picture of x.com aka Twitter logo" />
+        <img className='social-icons' src={LinkedInLogo}alt="A picture of linkedin logo" />
       </footer>
     </div>
     
